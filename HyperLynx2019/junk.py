45e78eaac3477d6244a76d0f741a2b05c991ec33
Jeff <wjstanek@gmail.com>
2019-03-09 11:31:32 -0700

    made pretty

--- HyperLynx2019/junk.py
+++ HyperLynx2019/junk.py
@@ -5,19 +5,19 @@ class Status:
     poll_raw = {'Brake_Pressure' : 0}
     poll_raw_q = {'Brake_Pressure' : []}
     poll_filter = {'Brake_Pressure' : 0}
-    moving_avg_count = 20
+    moving_avg_count = 10
 
 def poll_sensors():
 
-    PodStatus.poll_raw['Brake_Pressure'] = np.random.rand()
+    PodStatus.poll_raw['Brake_Pressure'] += 0.5*0.7*(time.clock()-timer)
 
     for key in PodStatus.poll_raw_q:
-        print(str(len(PodStatus.poll_raw_q[str(key)])))
+        #print(str(len(PodStatus.poll_raw_q[str(key)])))
         if len(PodStatus.poll_raw_q[str(key)]) < PodStatus.moving_avg_count:
-            print("Adding to q\n")
+            #print("Adding to q\n")
             PodStatus.poll_raw_q[str(key)] = np.append(PodStatus.poll_raw_q[str(key)], PodStatus.poll_raw[str(key)])
         else:
-            print("Shifting q\n")
+            #print("Shifting q\n")
             for i in range(0, (len(PodStatus.poll_raw_q[str(key)])-1)):
                 PodStatus.poll_raw_q[str(key)][i] = PodStatus.poll_raw_q[str(key)][i+1];
             PodStatus.poll_raw_q[str(key)][(PodStatus.moving_avg_count-1)] = PodStatus.poll_raw[str(key)];
@@ -29,7 +29,12 @@ if __name__ == '__main__':
     timer = time.clock()
     while True:
         poll_sensors()
-        print(PodStatus.poll_filter['Brake_Pressure'])
-        time.sleep(0.05)
-        if time.clock()-timer > 5:
+        time.sleep(0.000015)
+        if time.clock()-timer > 2:
+            print(PodStatus.poll_raw_q['Brake_Pressure'])
+            print(str(round(PodStatus.poll_filter['Brake_Pressure'],2)))
+            std_dev = np.std(PodStatus.poll_raw_q['Brake_Pressure'])
+            error = std_dev / PodStatus.poll_filter['Brake_Pressure']
+            print("Std dev: " + str(round(std_dev,3)))
+            print("% error: " + str(round(100*error,3)))
             break
