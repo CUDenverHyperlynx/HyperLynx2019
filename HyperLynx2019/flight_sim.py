0299386741674646446a26ab47c5e45588668287
Jeff <wjstanek@gmail.com>
2019-04-11 11:24:26 -0600

    updated flight_sim to use dict

--- HyperLynx2019/flight_sim.py
+++ HyperLynx2019/flight_sim.py
@@ -6,23 +6,23 @@ Flight simulator
 
 from time import clock
 
-def sim(pod_data, cmd_int, cmd_ext, *args):
-    if pod_data['state'] == 1:
+def sim(PodStatus, cmd_int, cmd_ext, *args):
+    if PodStatus.state == 1:
         #Do S2A stuff
         pass
-    elif pod_data['state'] == 3:
+    elif PodStatus.state == 3:
         # Do Launching stuff
         pass
-    elif pod_data['state'] == 5:
+    elif PodStatus.state == 5:
         # Do Brake1 stuff
         pass
-    elif pod_data['state'] == 6:
+    elif PodStatus.state == 6:
         # Do Crawling stuff
         pass
     else:
         # Do Brake2 stuff
         pass
 
-    return(pod_data)
+    return(PodStatus)
 
 
