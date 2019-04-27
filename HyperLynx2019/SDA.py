d8309d72546a2cb93207068cd200c6dcd5396c92
jrbre <john.brenner@ucdenver.edu>
2019-04-27 13:56:56 -0600

    SDA changes

--- HyperLynx2019/SDA.py
+++ HyperLynx2019/SDA.py
@@ -219,15 +219,15 @@ def init():
 
     PodStatus.cmd_int = {"Abort": 0,
                          "HV": 0,
-                         "Vent_Sol": 0,
-                         "Res1_Sol": 0,
+                         "Vent_Sol": 1,
+                         "Res1_Sol": 1,
                          "Res2_Sol": 0,
                          "MC_Pump": 0}
 
     PodStatus.cmd_ext = {"Abort": 0,
                          "HV": 0,
-                         "Vent_Sol": 0,
-                         "Res1_Sol": 0,
+                         "Vent_Sol": 1,
+                         "Res1_Sol": 1,
                          "Res2_Sol": 0,
                          "MC_Pump": 0}
 
