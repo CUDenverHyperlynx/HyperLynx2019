cb5e3b17663a9f8d80fbd447483b11faed0789e4
jrbre <john.brenner@ucdenver.edu>
2019-05-02 17:45:45 -0600

    SDA changes

--- HyperLynx2019/SDA.py
+++ HyperLynx2019/SDA.py
@@ -269,7 +269,7 @@ def poll_sensors():
     # If you want to run the flight sim:
     if PodStatus.flight_sim is True:
 
-        PodStatus.sensor_data['Brake_Pressure'] = PodStatus.sensor_poll.getBrakePressure()
+        #PodStatus.sensor_data['Brake_Pressure'] = PodStatus.sensor_poll.getBrakePressure()
         PodStatus.sensor_data['LVBatt_Temp'] = PodStatus.sensor_poll.getBatteryTemp()
         PodStatus.sensor_data['LVBatt_Current'] = PodStatus.sensor_poll.getCurrentLevel()
         PodStatus.sensor_data['LVBatt_Voltage'] = PodStatus.sensor_poll.getVoltageLevel()
