617de6e0bae73c1986923fcbf546f8d94ceafb0b
mosqueteiro <nat3.th3.gr3at@gmail.com>
2019-06-24 13:52:37 -0600

    ..

--- network_transfer/app-client.py
+++ network_transfer/app-client.py
@@ -9,62 +9,6 @@ import libclient
 
 sel = selectors.DefaultSelector()
 
-class BaseClient():
-    def __init_(self, host, port, action, value):
-        self.sel = selectors.DefaultSelector()
-        self.request = self._create_request(action, value)
-        self._start_connection:(host, port, self.request)
-        try:
-            while True:
-                events = self.sel.select(timeout=1)
-                for key, mask in events:
-                    message = key.data
-                    try:
-                        message.process_events(mask)
-                    except Exception:
-                        print(
-                            "main: error: exception for",
-                            f"{message.addr}:\n{traceback.format_exc()}",
-                        )
-                        message.close()
-                # Check for a socket being monitored to continue.
-                if not self.sel.get_map():
-                    break
-        except KeyboardInterrupt:
-            print("caught keyboard interrupt, exiting")
-        finally:
-            self.sel.close()
-
-    def _create_request(self, action, value):
-        if action == "search":
-            return dict(
-                type="text/json",
-                encoding="utf-8",
-                content=dict(action=action, value=value),
-            )
-        elif action == 'send_data':
-            return dict(
-                type="text/json",
-                encoding="utf-8",
-                content=dict(action=action, value=value)
-            )
-        else:
-            return dict(
-                type="binary/custom-client-binary-type",
-                encoding="binary",
-                content=bytes(action + value, encoding="utf-8"),
-            )
-
-    def _start_connection(self, host, port, request):
-        addr = (host, port)
-        print("starting connection to", addr)
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setblocking(False)
-        sock.connect_ex(addr)
-        events = selectors.EVENT_READ | selectors.EVENT_WRITE
-        message = libclient.Message(sel, sock, addr, request)
-        sel.register(sock, events, data=message)
-
 def create_request(action, value):
     if action == "search":
         return dict(
