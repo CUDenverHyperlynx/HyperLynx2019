2836e94fc49c934bebab4f512341fbd4613a7b6f
Jeff <wjstanek@gmail.com>
2019-07-14 21:54:22 -0700

    adding timeouts module for each state

---
+++ HyperLynx2019/timeouts.py
@@ -0,0 +1,12 @@
+# stores timeouts for each state
+
+
+def get(self):
+    self.timeouts[0] = 60
+    self.timeouts[1] = 999
+    self.timeouts[2] = 60
+    self.timeouts[3] = 180
+    self.timeouts[5] = 30
+    self.timeouts[6] = 180
+    self.timeouts[7] = 30
+    return(self.timeouts)
